7ccbabba964b79710831c280af9b9312db394991
kokpadonusam <130610034+kokpadonusam@users.noreply.github.com>
2024-12-02 10:08:34 +0100

    connect db

--- indextest.js
+++ indextest.js
@@ -35,3 +35,14 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
+async function connectDB() {
+    try {
+        client.connect();
+        console.log('Connected to MongoDB');
+        db1 = client.db(dbName);
+        // console.log(db1)
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        console.error(err.message)
+    }
+}
